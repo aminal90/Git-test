7163e219b0c9375f571515cc55e2d0d4d75e9aa0
Amin <Amin@Amin-PC>
2016-09-25 20:32:54 +0300

    123

--- lesson 20 git-test project/src/az/aminal/test/Main.java
+++ lesson 20 git-test project/src/az/aminal/test/Main.java
@@ -2,6 +2,6 @@ package az.aminal.test;
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("Hello from GIT-HUB!!!!!!!!!!!");
+		System.out.println("Hello from GIT-HUB!!!!!!");
 	}
 }
